887468019765da68bab20d6580d176fe4f65b8b0
Jason Yang <jason.yang@wonderful.com>
2021-07-24 13:07:07 -0700

    28 - add form to update products

---
+++ Advanced-React-master/sick-fits/frontend/components/UpdateProduct.js
@@ -0,0 +1,121 @@
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+import useForm from '../lib/useForm';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    query {
+      Product(where: { id: $id }) {
+        id
+        name
+        description
+        price
+      }
+    }
+  }
+`;
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  // 1. We need to get t he existing product
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+
+  // 2. We need to get the mutation to update the product
+  const [
+    updateProduct,
+    { data: updateData, error: updateError, loading: updateLoading },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION);
+  // 2.1 create some state for form inputs
+  const { inputs, handleChange, clearForm, resetForm } = useForm(data?.Product);
+  console.log(inputs);
+  if (loading) return <p>loading...</p>;
+
+  // 3. We need the form to handle the updates
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const res = await updateProduct({
+          variables: {
+            id: id,
+            name: inputs.name,
+            description: inputs.description,
+            price: inputs.price,
+          },
+        }).catch(console.error);
+        console.log(res);
+      }}
+    >
+      <DisplayError erorr={error || updateError} />
+
+      <fieldset disabled={updateLoading} aria-busy={updateLoading}>
+        <label htmlFor='name'>
+          Name
+          <input
+            type='text'
+            id='name'
+            name='name'
+            placeholder='Name'
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor='price'>
+          Price
+          <input
+            type='number'
+            id='price'
+            name='price'
+            placeholder='price'
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor='description'>
+          Description
+          <textarea
+            id='description'
+            name='description'
+            placeholder='description'
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+      </fieldset>
+
+      <button type='submit'>Update Product</button>
+
+      {/* <button type='button' onClick={clearForm}>
+        clear form
+      </button>
+
+      <button type='button' onClick={resetForm}>
+        reset form
+      </button> */}
+    </Form>
+  );
+}
